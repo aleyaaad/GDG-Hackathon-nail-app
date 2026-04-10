ea43c3a4a7d9c5e6e84ab399306cd290b6e07f51
aleyaaad <153777158+aleyaaad@users.noreply.github.com>
2026-04-10 00:58:16 -0400

    firebase setup

---
+++ nail-app/firebase/firebaseConfig.js
@@ -0,0 +1,22 @@
+// firebase/firebaseConfig.js
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNJGROY2Fa0LdNC-njGyY7F5fBuXOVyis",
+  authDomain: "nail-sizing-app.firebaseapp.com",
+  projectId: "nail-sizing-app",
+  storageBucket: "nail-sizing-app.firebasestorage.app",
+  messagingSenderId: "745276596539",
+  appId: "1:745276596539:web:42db6df1755a3a1955171e",
+  measurementId: "G-YLQDSEMW27"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
